01e9aee4fd4efdd8ea874d8ce2d7d8cb77af5d47
jiayouquba <1113438067@qq.com>
2023-12-06 00:59:06 +0800

    final

--- applications/comments/comment.py
+++ applications/comments/comment.py
@@ -1,7 +1,11 @@
 from applications.comments import comment_blue
 from models import CommentModel
-from flask import Flask, request, session, redirect
+from flask import Flask, request, session, redirect, render_template
 from extensions import db
 
 
+@comment_blue.route('/comment')
+def get_comment():
+
+    return render_template('new_login.html')
 
